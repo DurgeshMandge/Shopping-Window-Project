4c47e23f4c7356b0b98ed4dfa17390f9af5d936c
Durgesh Mandge <durgeshdmd4602@example.com>
2023-06-19 17:12:37 +0530

    updated

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,24 @@
-import Header2 from "./Header2";
+import { logoURL } from "../constants";
+import { Link } from "react-router-dom";
 
-const Header = ()=>{
-  return (
-    <>
-      <Header2 />
-    </>
+const Header2 =()=>{
+    return (
+        <>
+    <div className="container">
+    <header className="d-flex align-items-center flex-wrap justify-content-center  border-bottom">
+      <a href="/" className="d-flex align-items-center   me-md-auto text-dark text-decoration-none">
+        <img src={logoURL} alt="Shopify" />
+      </a>
+
+      <ul className="nav nav-pills ">
+        <li className="nav-item "><Link to="/" className="nav-link navOpt" aria-current="page">Home</Link></li>
+        <li className="nav-item"><Link to="/about" className="nav-link navOpt">About</Link></li>
+        <li className="nav-item"><Link to="/contact" className="nav-link navOpt">Contact</Link></li>
+      </ul>
+    </header>
+  </div>
+  </>
   );
 }
 
-export default Header;
+export default Header2;
