0c2a7231308561bdd2e8c5056f097a8eff4991c0
Durgesh Mandge <durgeshdmd4602@example.com>
2023-06-19 16:48:11 +0530

    update

--- components/Contact.js
+++ components/Contact.js
@@ -14,9 +14,10 @@ const ContactLayout = ()=>{
                     <h5 className="card-title">Durgesh Mandge</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <Link to="/Contact/portfolio" className="btn btn-primary">View Portfolio</Link>
-            </div>
+                </div>
             </div>
         </div>
+        <hr/>
         <div className="contaier">
             <Outlet className="col-12" />
         </div>
