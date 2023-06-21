509c7a96f8e2f4498e9c15a732c15e87740525fb
Durgesh Mandge <durgeshdmd4602@example.com>
2023-06-21 10:55:37 +0530

    d

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,20 +5,22 @@ import { Link } from "react-router-dom";
 const LandingPageComponent = ({name,availability,status})=>{
     const rout="/"+name;
     return (
-    <Link to={rout} className="text-decoration-none link-black link-land compoList">
-    <div className=" border-dark border rounded p-1 link-black">
-        <div className="row align-items-center landImgComp">   
-            <div className="col-2 text-center">
-                <img src={arrowURL} placeholder="img.jpg" className="landImg" />
-            </div>
-            <div className="col-10">
-                    <h2 title="click here">{name}</h2>
-                    <h4>{availability}</h4>
-                    <h6>{status}</h6> 
+    <div className="compoList">
+        <Link to={rout} className="text-decoration-none link-black link-land ">
+        <div className=" border-dark border rounded p-1 link-black">
+            <div className="row align-items-center landImgComp">   
+                <div className="col-2 text-center">
+                    <img src={arrowURL} placeholder="img.jpg" className="landImg" />
+                </div>
+                <div className="col-10">
+                        <h2 title="click here">{name}</h2>
+                        <h4>{availability}</h4>
+                        <h6>{status}</h6> 
+                </div>
             </div>
         </div>
+        </Link>
     </div>
-    </Link>
     );
 }
 
