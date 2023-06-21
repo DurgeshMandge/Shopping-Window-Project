1a676712d86f0c83d94d648567d1cc94f9a0d875
Durgesh Mandge <durgeshdmd4602@example.com>
2023-06-21 10:20:48 +0530

    update

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const LandingPageComponent = ({name,availability,status})=>{
     const rout="/"+name;
     return (
-    <Link to={rout} className="container text-decoration-none link-black">
+    <Link to={rout} className="container text-decoration-none link-black link-land">
     <div className=" border-dark border rounded p-1 link-black">
         <div className="row align-items-center landImgComp">   
             <div className="col-2 text-center">
